7ae24087f8a8de1a0d079c61ffe639157391d11c
Daniel Badinszky <badinszky97@gmail.com>
2024-10-13 08:16:59 +0200

    Projekt létrehozva

---
+++ src/main/java/hu/nje/javabeadando/JavaBeadandoApplication.java
@@ -0,0 +1,13 @@
+package hu.nje.javabeadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBeadandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBeadandoApplication.class, args);
+	}
+
+}
